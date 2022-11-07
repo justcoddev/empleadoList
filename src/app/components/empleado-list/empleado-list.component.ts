f922f5ed27a6f6664875bf4cfb307c8704dd3eb1
justcoddev <edisonpaulcrz@gmail.com>
2022-11-07 17:46:10 -0500

    tabla

--- src/app/components/empleado-list/empleado-list.component.ts
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Empleado } from 'src/app/models/Empleado';
 
 @Component({
   selector: 'app-empleado-list',
@@ -6,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empleado-list.component.css']
 })
 export class EmpleadoListComponent implements OnInit {
+
+  listEmpleados: Empleado[] = [
+    {
+      legajo: 1, nombre: 'Alex', apellido: 'Contento', sexo: 'Masculino', salario: 22000
+    },
+    {
+      legajo: 2, nombre: 'Edison', apellido: 'Cruz', sexo: 'Masculino', salario: 26000
+    },
+    {
+      legajo: 3, nombre: 'Luis', apellido: 'Loayza', sexo: 'Masculino', salario: 26000
+    },
+    {
+      legajo: 4, nombre: 'Jorge', apellido: 'Santana', sexo: 'Masculino', salario: 22000
+    },
+    {
+      legajo: 5, nombre: 'Marlon', apellido: 'Villamar', sexo: 'Masculino', salario: 22000
+    },
+    {
+      legajo: 6, nombre: 'Paulina', apellido: 'Zeins', sexo: 'Femenino', salario: 24000
+    }
+  ]
 
   constructor() { }
 
